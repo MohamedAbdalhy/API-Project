bf04a8577d671395c2eaa6946dfd93783aa3c665
Mohamed <mohamedabdalhy15@gmail.com>
2023-03-04 21:55:54 +0200

    dep

--- src/test/java/com/learn/LearnTestApi.java
+++ src/test/java/com/learn/LearnTestApi.java
@@ -1,7 +1,21 @@
 package com.learn;
 
-import io.restassured.RestAssured;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+ import static  io.restassured.RestAssured.*;
+import static    io.restassured.matcher.RestAssuredMatchers.*;
+import static   org.hamcrest.Matchers.*;
 
 public class LearnTestApi {
-    RestAssured.
+ @Test
+    public void test1(){
+    given().baseUri("https://qacart-academy-api.herokuapp.com")
+             .when().get("/api/v1/info/courses")
+             .then().log().all();
+
+
+ }
+
+
 }
